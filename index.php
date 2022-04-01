a3d6ce6818c2578c872706e01fb06fc522a00e3c
riinaaamur <riinaamur@gmail.com>
2022-04-01 17:51:04 +0300

    доработка задания Итерация по массивам

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Карина';
 ?>
+<?php $category = [
+    "Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"
+];
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -48,50 +52,45 @@ $user_name = 'Карина';
         </div>
     </header>
 
-    <?php $category = [
-        "«Доски и лыжи» ", "«Крепления» ", "«Ботинки» ", "«Одежда» ", "«Инструменты» ", "«Разное»"
-    ];
-    ?>
-    <?php $ads1 = [
+
+    <?php
+    $lots = [
+        [
         'title' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 10999,
         'url' => 'img/lot-1.jpg'
-    ];
-    ?>
-    <?php $ads2 = [
+        ],
+        [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
         'url' => 'img/lot-2.jpg'
-    ];
-    ?>
-    <?php $ads3 = [
+        ],
+        [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 8000,
         'url' => 'img/lot-3.jpg'
-    ];
-    ?>
-    <?php $ads4 = [
+         ],
+        [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
         'url' => 'img/lot-4.jpg'
-    ];
-    ?>
-    <?php $ads5 = [
+        ],
+        [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
         'url' => 'img/lot-5.jpg'
-    ];
-    ?>
-    <?php $ads6 = [
+        ],
+        [
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
         'url' => 'img/lot-6.jpg'
+    ]
     ];
     ?>
     <main class="container">
@@ -107,19 +106,18 @@ $user_name = 'Карина';
                 <h2>Открытые лоты</h2>
             </div>
             <ul class="lots__list">
-
-
+        <?php foreach ($lots as $key => $value): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="img/lot-1.jpg" width="350" height="260" alt="">
+                        <img src="<?=$value['url']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$ads1['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ads1['title'];?></a></h3>
+                        <span class="lot__category"><?=$lots['category'];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lots['title'];?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$ads1['price'];?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=$lots['price'];?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
@@ -127,96 +125,7 @@ $user_name = 'Карина';
                         </div>
                     </div>
                 </li>
-                <li class="lots__item lot">
-                    <div class="lot__image">
-                        <img src="img/lot-2.jpg" width="350" height="260" alt="">
-                    </div>
-                    <div class="lot__info">
-                        <span class="lot__category"><?=$ads2['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ads2['title'];?></a></h3>
-                        <div class="lot__state">
-                            <div class="lot__rate">
-                                <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$ads2['price'];?><b class="rub">р</b></span>
-                            </div>
-                            <div class="lot__timer timer">
-                                12:23
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="lots__item lot">
-                    <div class="lot__image">
-                        <img src="img/lot-3.jpg" width="350" height="260" alt="">
-                    </div>
-                    <div class="lot__info">
-                        <span class="lot__category"><?=$ads3['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ads3['title'];?></a></h3>
-                        <div class="lot__state">
-                            <div class="lot__rate">
-                                <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$ads3['price'];?><b class="rub">р</b></span>
-                            </div>
-                            <div class="lot__timer timer">
-                                12:23
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="lots__item lot">
-                    <div class="lot__image">
-                        <img src="img/lot-4.jpg" width="350" height="260" alt="">
-                    </div>
-                    <div class="lot__info">
-                        <span class="lot__category"><?=$ads4['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ads4['title'];?></a></h3>
-                        <div class="lot__state">
-                            <div class="lot__rate">
-                                <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$ads4['price'];?><b class="rub">р</b></span>
-                            </div>
-                            <div class="lot__timer timer">
-                                12:23
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="lots__item lot">
-                    <div class="lot__image">
-                        <img src="img/lot-5.jpg" width="350" height="260" alt="">
-                    </div>
-                    <div class="lot__info">
-                        <span class="lot__category"><?=$ads5['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ads5['title'];?></a></h3>
-                        <div class="lot__state">
-                            <div class="lot__rate">
-                                <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$ads5['price'];?><b class="rub">р</b></span>
-                            </div>
-                            <div class="lot__timer timer">
-                                12:23
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="lots__item lot">
-                    <div class="lot__image">
-                        <img src="img/lot-6.jpg" width="350" height="260" alt="">
-                    </div>
-                    <div class="lot__info">
-                        <span class="lot__category"><?=$ads6['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ads6['title'];?></a></h3>
-                        <div class="lot__state">
-                            <div class="lot__rate">
-                                <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$ads6['price'];?><b class="rub">р</b></span>
-                            </div>
-                            <div class="lot__timer timer">
-                                12:23
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                <?php endforeach; ?>
             </ul>
         </section>
     </main>
