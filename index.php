b8c3de6358426278dc53ce59a2548701e3c19227
riinaaamur <riinaamur@gmail.com>
2022-04-01 18:22:04 +0300

    доработка задания Итерация по массивам

--- index.php
+++ index.php
@@ -36,7 +36,8 @@ $user_name = 'Карина';
                     <p>Карина</p>
                     <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                     <a class="user-menu__logout" href="#">Выход</a>
-                </div></p>
+                </div>
+
 
             <?php else: ?>
                 <p><ul class="user-menu__list">
@@ -46,7 +47,7 @@ $user_name = 'Карина';
                     <li class="user-menu__item">
                         <a href="#">Вход</a>
                     </li>
-                </ul></p>
+                </ul>
                 <?php endif; ?>
             </nav>
         </div>
@@ -112,12 +113,12 @@ $user_name = 'Карина';
                         <img src="<?=$value['url']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$lots['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lots['title'];?></a></h3>
+                        <span class="lot__category"><?=$value['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['title']; ?></a></h3>
                         <div class="lot__state">
-                            <div class="lot__rate">
+                            <div class="lot__amount">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$lots['price'];?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=$value['price']; ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
